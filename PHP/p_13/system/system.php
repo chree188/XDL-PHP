310d785420118abf89b1eabba36ced4584d2c293
gitzwt <zwt0706@126.com>
2016-07-05 15:41:12 +0800

    7.5 update1

--- PHP/p_13/system/system.php
+++ PHP/p_13/system/system.php
@@ -3,9 +3,31 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>文件管理系统</title>
+		<?php	
+			header('content-type:text/html;charset=utf-8');
+			$GETa = empty($_GET['a']) ? " " : $_GET['a'];
+		?>
 	</head>
-	<body>
-		<h2><a href="system.php">文件管理系统</a></h2>
+	<body >
+		
+		<?php	
+//			对文件执行 添加 删除 修改的操作		
+			switch ($GETa) {
+				case 'create':	//添加
+//					使用file_put_content添加内容到文件中的函数实现新建文件
+					file_put_contents($_POST['filename'], "");
+					break;
+				case 'update':	//修改
+				file_put_contents($_POST['filename'], $_POST['content']);
+//				将值1写入到文件1中以追加写的方式
+//				file_put_contents(文件1, 值1,[FILE_APPEND]);
+					break;
+				case "del":	//删除
+					@unlink($_GET['filename']);
+			}
+		?>
+		
+		<h2><a href="system.php" style="text-decoration: none;">文件管理系统</a></h2>
 		
 		<!--表格开始-->
 		<table width="600px" border="1px">
@@ -47,21 +69,33 @@
 		</table>
 		<!--表格结束-->
 		
-		<h4><a href="system.php?a=add">新建文件</a></h4>
+		<h4><a href="system.php?a=add" style="text-decoration: none;">新建文件</a></h4>
 		
 		<?php	
-			$_GET['a'] = empty($_GET['a'] ? " " : $_GET['a']);
-//			显示添加菜单
-			switch ($_GET['a']) {
+			
+			switch ($GETa) {
+//				显示新建文件
 				case 'add':
+//					第一种表格输出形式
 					echo "<form action='system.php?a=create' method='post'>";
 						echo "<input type='text' name='filename'>";
 						echo "<input type='submit' value='新建'>";
 					echo "</form>";
 					break;
-				
-				default:
-					
+//				显示修改表单
+				case "edit":
+//					第二种表格输出方式 定界符输出表格
+// 					1 <<<aa后面不能有空格
+// 					2 aa; aa要定格写
+// 					3 $str要echo 输出
+					$ycontent=file_get_contents($_GET['filename']);	//读取文件内容
+					echo "<h3>修改的文件为：{$_GET['filename']}</h3>";
+					echo "<form action='system.php?a=update' method='post'>";
+						echo "<input type='hidden' name='filename' value='{$_GET['filename']}'>";
+						echo "<textarea name='content' cols='30' rows='10'>{$ycontent}</textarea><br>";
+						echo "<input type='submit' value='修改'>";
+					echo "</form>";
+
 					break;
 			}
 		?>
