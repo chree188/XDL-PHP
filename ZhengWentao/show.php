f18efb5b47e6f862f31b76cc060520b10de6abaf
gitzwt <zwt0706@126.com>
2016-07-10 00:37:05 +0800

    update
    update

---
+++ ZhengWentao/show.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML >
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>留言板</title>
+	</head>
+	<body>
+		<center>
+			<form action="" method="post" enctype="multipart/form-data">
+				<h2>文件式留言板</h2>
+				<a href="liuyan.php">添加留言</a>|<a href="show.php">查看留言</a>
+				<hr>
+				<h3>查看留言</h3>
+				<table border="1" width="300px">
+					<tr>
+						<th>标题</th>
+						<th>作者</th>
+						<th>内容</th>
+						<th>IP</th>
+						<th>操作</th>
+					</tr>
+				</table>
+			</form>
+		</center>
+	</body>
+</html>
