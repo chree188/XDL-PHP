512db7903b06c928815c082047c309ecc42785ae
gitzwt <zwt0706@126.com>
2016-07-21 10:04:20 +0800

    7.21 update

--- PHP/MySQL_PHP/StuMg/menu.php
+++ PHP/MySQL_PHP/StuMg/menu.php
@@ -5,5 +5,9 @@
 			color: yellowgreen;
 		}
 	</style>
-	<h3><a href="index.php">查看学生信息</a> | <a href="add.php">添加学生信息</a></h3>
+	<h3>
+		<a href="add.php">添加学生信息</a> | <a href="index.php">查看学生信息</a> |
+		 <a href="index1.php">分页查看学生信息</a> | <a href="index2.php">查看学生信息</a> |
+		  <a href="index3.php">查看学生信息</a> |
+	</h3>
 	<hr>
