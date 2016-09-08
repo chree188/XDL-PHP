3c046af31caf7a6dbb0c3ea2dcc0230b6a44ccb5
gitzwt <zwt0706@126.com>
2016-09-08 18:28:40 +0800

    9.8 update1

--- OOP/oop04/4-2.php
+++ OOP/oop04/4-2.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: hui
  * Date: 2016/9/8
- * Time: 14:38
+ * Time: 18:09
  */
 
 header("content-type:text/html;charset=utf-8");
@@ -11,14 +11,14 @@ header("content-type:text/html;charset=utf-8");
 static 静态的
     定义:用于修饰属性和方法,不能修饰类
     修饰的属性:静态属性
-    修饰的方法:静态方法 亦可以使用动态的方式去调用(不建议)
+    修饰的方法:静态方法 亦可使用动态的方法去调用(不建议)
 在外部访问静态属性/方法:
     类名::$属性名
     类名::方法名();
 在内部访问静态属性/方法:
     self::$属性名
     self::方法名()
-如果一个方法内部  没有动态的内容,默认把该方法当作静态方法(即使没有static关键字)
+如果一个方法内部 没有动态的内容,默认把该方法当作静态方法(即使没有static关键字)
 静态方法中,不允许出现动态的内容$this(非静态)
 */
 
@@ -27,7 +27,7 @@ class Person
     public static $name = '安娜';
     private static $age = 18;
 
-    public  static function demo()
+    public static function demo()
     {
         echo '啊,我司静态方法!<br>';
     }
@@ -40,27 +40,27 @@ class Person
         return self::$age;  //在内部访问静态属性
     }
 
-//    普通方法
+//  普通方法
     public function fun1()
     {
-        echo '啊,我司fun1...<br>';
+        echo '啊,我司fun1<br>';
     }
     public static function fun2()
     {
-        echo '俺是fun2...<br>';
+        echo '俺是fun2<br>';
 //        var_dump($this);
     }
 }
 
 //$p = new Person();
 //$p->demo(); //不建议
-//echo 'name'.$p->name.'<br>';    //动态的
-//静态属性的访问
+//echo 'name:'.$p->name.'<br>';   //动态的
+
 echo '姓名:'.Person::$name.'<br>';
 Person::demo();
 
 echo '<hr>';
-//echo 'AGE:'.Person::$age;   //被3P所限制
+//echo 'AGE:'.Person::$age;   //被3p限制
 echo '年龄:'.Person::getAge();
 
 echo '<hr>';
