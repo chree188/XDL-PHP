6996fa7d4332fc4441c3bb685ddfa0810e6c2b55
gitzwt <zwt0706@126.com>
2016-09-12 16:01:32 +0800

    9.12 update1

--- OOP/oop05/4-2.php
+++ OOP/oop05/4-2.php
@@ -5,3 +5,22 @@
  * Date: 2016/9/12
  * Time: 15:01
  */
+
+header("content-type:text/html;charset=utf-8");
+
+/*__invoke()
+    把对象当作函数去调用时,自动触发
+    1.类似于tostring
+    2.把对象当作函数去调用
+*/
+
+class A
+{
+    public function __invoke()
+    {
+        echo '啊,我被调用了<br>';
+    }
+}
+
+$p = new A();
+$p();   //把对象当作函数一样去调用
---
+++ OOP/oop05/5-1.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/12
+ * Time: 15:07
+ */
+
+header("content-type:text/html;charset=utf-8");
+
+/*__call()
+    当调用 不存在的方法时 自动触发
+    参1,调用的不存在的静态方法名
+    参2,调用的不存在的静态方法的参数
+
+__callStatic()
+    当调用 不存在的静态方法时 自动触发
+    参1,调用的不存在的静态方法名
+    参2,调用的不存在的静态方法的参数
+*/
+class Person
+{
+    public  function say()
+    {
+        echo 'I say....<br>';
+    }
+    public function eat()
+    {
+        echo 'I eat...<br>';
+    }
+    public function __call($funName, $params)
+    {
+        echo '<pre>';
+        echo '啊,我被call了<br>';
+        print_r($funName);
+        echo '<br>';
+        print_r($params);
+        echo '</pre>';
+    }
+    public static function __callStatic($funName, $params)
+    {
+        echo '<pre>';
+        echo '啊,我也被call了<br>';
+        print_r($funName);
+        echo '<br>';
+        print_r($params);
+        echo '</pre>';
+    }
+}
+
+$p = new Person();
+$p->
