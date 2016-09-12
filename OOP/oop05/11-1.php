0d76fb2635b01b78dcf592b2db9e452500ae14d5
gitzwt <zwt0706@126.com>
2016-09-12 21:38:08 +0800

    9.12 update4

---
+++ OOP/oop05/11-1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/12
+ * Time: 20:00
+ */
+
+header("content-type:text/html;charset=utf-8");
+
+//总结 所有的魔术方法
+/**
+public function __construct(){}
+public function __destruct(){}
+
+public function __set($key,$value){$this->$key = $value;}
+public function __get($key){return $this->$key;}
+
+public function __isset($key){return isset($this->$key);}
+public function __unset($key){unset($this->$key);}
+
+public function __tostring(){return "string..."}
+public function __invoke(){}
+
+public function __call($funName,$params){}
+public static function __callStatic($funName,$params){}
+
+public function __sleep(){return array();}
+public function __wakeup(){}
+
+public function __clone(){}
+function __autoload($classname){}
+**/
