972e650133fa39eef7d95b23e664bb03aa281467
gitzwt <zwt0706@126.com>
2016-09-14 15:12:24 +0800

    9.14 update1

--- OOP/oop07/3-2.php
+++ OOP/oop07/3-2.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: hui
  * Date: 2016/9/14
- * Time: 14:46
+ * Time: 14:51
  */
 
 header("content-type:text/html;charset=utf-8");
@@ -12,7 +12,7 @@ header("content-type:text/html;charset=utf-8");
 try {
 //    实例化PDO 参数:DSN User Pass
 //    DSN:数据源名称 就是数据库连接的信息
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=s50','root','zwt12345');
+    $pdo = new  PDO('mysql:host=127.0.0.1;dbname=s50','root','zwt12345');
     var_dump($pdo);
 } catch (PDOException $e) {
     echo $e->getMessage();
---
+++ OOP/oop07/pdoconfig.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/14
+ * Time: 14:57
+ */
+
+//'mysql:host=127.0.0.1;dbname=s50','root','zwt12345'
+const DSN = 'mysql:host=127.0.0.1;dbname=s50';
+const USER = 'root';
+const PASS = 'zwt12345';
