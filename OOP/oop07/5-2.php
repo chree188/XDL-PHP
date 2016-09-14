50e7c7915bea00b49128212c1934e39bdd28932d
gitzwt <zwt0706@126.com>
2016-09-14 15:59:15 +0800

    9.14 update2

---
+++ OOP/oop07/5-2.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/14
+ * Time: 15:20
+ */
+
+header("content-type:text/html;charset=utf-8");
+require 'pdoconfig.php';
+/*
+PDO 设置SQL错误处理方式
+
+属性: PDO::ATTR_ERRMODE
+值: PDO::ERRMODE_SILENT(默认) 瞎了
+    PDO::ERRMODE_WARNING 报错,警告
+    PDO::ERRMODE_EXCEPTION 抛出异常
+*/
+
+//使用PDO
+try {
+//    实例化PDO
+    $pdo = new PDO(DSN,USER,PASS);
+//    设置属性
+//    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);  //默认
+//    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING); //错误提示
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);   //异常
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+try {
+    $sql = "SELECT * FROM aabbcc";
+    $pdo->query($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
---
+++ OOP/oop07/7-1.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/14
+ * Time: 15:36
+ */
+
+header("content-type:text/html;charset=utf-8");
+require 'pdoconfig.php';
+/*
+PDO 查
+    query() 查询 返回的是PDOStatement的实例
+    fetch() 单条数据/一维数组
+    fetchAll() 所有数据/二维数组
+结果集属性的设置
+    PDO::ATTR_DEFAULT_FETCH_MODE
+    值:
+        PDO::FETCH_ASSOC 关联
+        PDO::FETCH_NUM 索引
+        PDO::FETCH_OBJ  对象
+        PDO::FETCH_BOTH 混合
+*/
+
+//使用PDO
+try {
+//    实例化PDO
+    $pdo = new PDO(DSN,USER,PASS);
+//    设置字符集
+    $pdo->query("set names utf8");
+//    设置属性
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//    设置结果集返回数组的类型
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);  //关联
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_NUM);    //索引
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);    //对象
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_BOTH);   //混合
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+try {
+    $sql = "SELECT * FROM user";
+    $stmt = $pdo->query($sql);
+//    var_dump($stmt);    //返回一个对象
+//    单条数据/首条数据/一位数组
+//    $rows = $stmt->fetch();
+//    所有数据/二维数组
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($rows);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
