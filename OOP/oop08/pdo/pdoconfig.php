b6b95ddb760bbf1dab0d9982b8f59a5c3fae2cfe
gitzwt <zwt0706@126.com>
2016-09-18 14:21:40 +0800

    9.18 update

---
+++ OOP/oop08/pdo/pdoconfig.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/9/18
+ * Time: 13:39
+ */
+
+const DSN = 'mysql:host=127.0.0.1;dbname=s50';
+const USER = 'root';
+const PASS = 'zwt12345';
