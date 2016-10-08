411ed47a78edec2f1a319fd05c261fd4c7e0db7d
gitzwt <zwt0706@126.com>
2016-10-08 19:46:23 +0800

    10.8 update3

---
+++ JavaScript/js11/2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/10/8
+ * Time: 18:06
+ */
+
+echo $_GET['a'] + $_GET['b'];
---
+++ JavaScript/js11/3.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2016/10/8
+ * Time: 19:28
+ */
+
+echo $_POST['a'] + $_POST['b'];
