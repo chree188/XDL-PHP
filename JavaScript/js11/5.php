a7f6b48e79ffaca0ce48f3a7b39ca64193f2ef8f
gitzwt <zwt0706@126.com>
2016-10-08 21:58:56 +0800

    10.8 update5

--- JavaScript/js11/5.php
+++ JavaScript/js11/5.php
@@ -3,16 +3,14 @@
  * Created by PhpStorm.
  * User: hui
  * Date: 2016/10/8
- * Time: 20:06
+ * Time: 20:50
  */
+
+header('content-type:text/html;charset=utf-8');
 
 //导入
 require 'config.php';
 require 'Model.class.php';
 
 $model = new Model('user');
-$data = $model->select();
-//var_dump($data);
-
-//将数据转化为json编码
-echo json_encode($data);
+echo $model->add();//执行插入
