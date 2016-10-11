860dcf274ce6155b19179f8fc65d77d6fc32bc23
gitzwt <zwt0706@126.com>
2016-10-11 22:12:14 +0800

    10.11 update2

---
+++ JavaScript/js14/p/1.php
@@ -0,0 +1,10 @@
+<?php 
+header("content-type:text/html;charset=utf-8");
+
+// $a = 1+1;
+echo 'hello,ajax '.date('H:i:s');
+
+
+
+
+
---
+++ JavaScript/js14/p/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('HOST','localhost');
+define('USER','root');
+define('PWD','123456');
+define('DB','s51');
+define('CHAR','utf8');
+
+
+
+
