c3d9dc0e77d41417ce3c1f9ba3996928fdeca3d6
gitzwt <zwt0706@126.com>
2016-10-14 15:42:28 +0800

    10.14 update2

--- HYBBS/Tmp/log.php
+++ HYBBS/Tmp/log.php
@@ -1,3 +1,7 @@
 <?php die(); ?>不存在该页面  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\function.php #行数: 199 #发生时间: 2016-10-09 09:23:26
 不存在该页面  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\function.php #行数: 199 #发生时间: 2016-10-09 09:23:26
 不存在该页面  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\function.php #行数: 199 #发生时间: 2016-10-09 09:23:32
+不存在该页面  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\function.php #行数: 199 #发生时间: 2016-10-14 11:56:53
+不存在该页面  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\function.php #行数: 199 #发生时间: 2016-10-14 11:57:09
+错误类型(警告) : file_put_contents(D:/PHP/wamp64/www/HYBBS/Tmp/AdminAction_7305a22810c94cd5fcdc5a42219908c1.php): failed to open stream: Invalid argument  #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\HY.lib.php #行数: 176 #发生时间: 2016-10-14 11:57:21
+错误类型(警告) : file_get_contents():   #发生错误的文件位于: D:\PHP\wamp64\www\HYBBS\HY\lib\HY.lib.php #行数: 176 #发生时间: 2016-10-14 11:57:21
