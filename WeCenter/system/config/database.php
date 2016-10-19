dd691de2ca73a26f411743636445bfd272aec0af
gitzwt <zwt0706@126.com>
2016-10-19 22:29:38 +0800

    10.19 update3 添加wecenter Demo项目

---
+++ WeCenter/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'zwt12345',
+  'dbname' => 'we',
+);
+$config['slave'] = false;
